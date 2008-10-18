8770cef904c688b9957faff9bd16be146e53b74d
Joon Lee <joon@kimlee.org>
2008-10-18 00:07:50 -0400

    added citation helper

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,7 @@
 module ArticlesHelper
+  def citation(article)
+    # http://en.wikipedia.org/wiki/Vancouver_system
+    citation = "#{article.journal}. #{article.year};#{article.volume}(#{article.issue}):#{article.pagination}."
+    content_tag :div, citation, :class => "citation"
+  end
 end
