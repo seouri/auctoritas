19bfc5149adb66100cc697985b103e40d613ee59
Joon Lee <joon@kimlee.org>
2008-10-19 12:42:54 -0400

    show 10 articles in the index

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ActiveRecord::Base
   def self.search(query, options = {})
-    options[:per_page] ||= 20
+    options[:per_page] ||= 10
     options[:page] ||= 1
     paginate options
   end
