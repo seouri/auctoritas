5a2301192fdca5cb046b259dcbc0590c0ff286c6
Joon Lee <joon@leelaboratory.com>
2008-10-20 12:21:06 -0400

    take care of missing issue

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,7 +1,8 @@
 module ArticlesHelper
   def citation(article)
     # http://en.wikipedia.org/wiki/Vancouver_system
-    citation = "#{article.journal}. #{article.year};#{article.volume}(#{article.issue}):#{article.pagination}."
+    issue = article.issue.empty? ? "" : "(#{article.issue})"
+    citation = "#{article.journal}. #{article.year};#{article.volume}#{issue}:#{article.pagination}."
     content_tag :div, citation, :class => "citation"
   end
   
