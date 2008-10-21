30d72c1e3b95e14128bc371fa71312979b0ba3c5
Joon Lee <joon@kimlee.org>
2008-10-21 09:13:54 -0400

    wired article with authors directly

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ActiveRecord::Base
   has_many :authorships, :include => :author
+  has_many :authors, :through => :authorships
   
   def self.search(query, options = {})
     options[:per_page] ||= 10
