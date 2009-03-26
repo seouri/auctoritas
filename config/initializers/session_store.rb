0a471ff8af0191630070616c85e86de6b3ac47f5
Joon Lee <seouri@gmail.com>
2009-03-26 10:19:03 -0400

    upgraded to Rails 2.3.2

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,8 +5,8 @@
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
-  :key         => '_auctoritas_session',
-  :secret      => 'c036c433ef489cdb2424401d1652a0a583af31e6f36b1af3f2f6f30d9476d48b6ef48cdb24121324d7985080ff8169d131a12d4143a018f27f273a197c70a77d'
+  :key         => '_t_session',
+  :secret      => 'f011309d4801d1ab3d96bd8a6616ce77e4f2171a0219dedc6906c858e8c8e6c2a685226fca2b61f191b53d079d20cfd9edd85c7e35e208fefcb57256da6d947a'
 }
 
 # Use the database for sessions instead of the cookie-based default,
