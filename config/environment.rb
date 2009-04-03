cb1021ca1a6991fbc33165cabe0b5f9554535f25
Joon Lee <seouri@gmail.com>
2009-04-03 10:42:00 -0400

    installed ruby-openid-2.1.4 gem

--- config/environment.rb
+++ config/environment.rb
@@ -22,6 +22,7 @@ Rails::Initializer.run do |config|
   config.gem 'mislav-will_paginate', :version => '~> 2.3.8', :lib => 'will_paginate', :source => 'http://gems.github.com'
   config.gem "rspec", :lib => false, :version => ">= 1.2.2" 
   config.gem "rspec-rails", :lib => false, :version => ">= 1.2.2"
+  config.gem "ruby-openid", :version => ">= 2.1.4", :lib => "openid"
   config.gem "authlogic", :version => ">= 2.0.5"
   config.gem "authlogic-oid", :version => ">= 1.0.2", :lib => "authlogic_openid"
 
