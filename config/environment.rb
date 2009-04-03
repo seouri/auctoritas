2e0924e101f8c8dd946c6e5ced9c8a837470dd26
Joon Lee <seouri@gmail.com>
2009-04-03 08:40:37 -0400

    installed rspec and rspec-rails

--- config/environment.rb
+++ config/environment.rb
@@ -20,6 +20,8 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem 'mislav-will_paginate', :version => '~> 2.3.8', :lib => 'will_paginate', :source => 'http://gems.github.com'
+  config.gem "rspec", :lib => false, :version => ">= 1.2.2" 
+  config.gem "rspec-rails", :lib => false, :version => ">= 1.2.2"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
